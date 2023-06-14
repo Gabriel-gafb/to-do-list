25f3858a07a6193d7bfde9ef2c373bf303c4a72d
Gabriel Augusto <gafb.xpenergy@gmail.com>
2023-06-13 22:49:50 -0300

    feat: InputBoard layout and start TaskBoard layout

--- src/components/InputBoard.tsx
+++ src/components/InputBoard.tsx
@@ -1,12 +1,16 @@
 import styles from './InputBoard.module.css'
-
+import {PlusCircle} from '@phosphor-icons/react'
 export function InputBoard(){
 
     return(
         <div>
             <form className={styles.inputForm}>
-                <input placeholder="Teste"/>
-                <button/>
+                <input placeholder="Adicione uma nova tarefa"/>
+                <button>
+                    <span>Criar</span>
+                    <PlusCircle size={20}/>
+
+                </button>
             </form>
         </div>
     )
--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -3,7 +3,37 @@ import styles from './TaskBoard.module.css';
 export function TaskBoard(){
     return (
         <div className={styles.taskList}>
-            TaskBoard
+            <div className={styles.infos}>
+                <div>
+                    <strong>Tarefas criadas</strong>
+                    <span>5</span>
+                </div>
+                <div>
+                    <strong> Concluidas</strong>
+                    <span>2 de 5</span>
+                </div>
+            </div>
+                <div className={styles.registeredTaskBoard}>
+                    Teste
+                    {/* <ul>
+                        <li>
+                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                        </li>
+                        <li>
+                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                        </li>
+                        <li>
+                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                        </li>
+                        <li>
+                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                        </li>
+                        <li>
+                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                        </li>
+                    </ul> */}
+                </div>
+
         </div>
     )
 }
